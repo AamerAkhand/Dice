cec97ea76e9f0ad2aa3c9f0bc87e7f1563d32927
Aamer <aakhand@uwaterloo.ca>
2025-10-05 17:04:37 -0400

    Risk

--- characters.py
+++ characters.py
@@ -110,7 +110,7 @@ DICE_LIBRARY = {
         'description': 'Consistent 4 movement'
     },
     'risk': {
-        'values': [-3, -2, 8, 8],
+        'values': [-3, -3, 8, 8],
         'label': 'Risk',
         'description': 'High risk, high reward (-2 or 8)'
     },
